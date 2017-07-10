259613640655653dbcffe00057e01354cc29203b
Matthew Collis <matthew.collis@mail.mcgill.ca>
2017-07-10 17:45:06 -0400

    priority migration chang: transactions --> trades

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -1,7 +1,7 @@
 class Jam < ApplicationRecord
   belongs_to :user
-  has_many :comment dependent: :destroy
+  has_many :comment, dependent: :destroy
   has_one :transaction
-  
+
   validates :name, presence: true, uniqueness: true
 end
