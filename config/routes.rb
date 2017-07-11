f51f145ebc8bccae0d26e4a10c89324723bcf309
Matthew Collis <matthew.collis@mail.mcgill.ca>
2017-07-11 10:45:08 -0400

    changes to index and its scss + minor others

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments, only: [:destroy]
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'jams#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
